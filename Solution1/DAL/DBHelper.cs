b66418265d23f31cd799006db28d754e05a8d04f
yxk123450 <1129276116@qq.com>
2016-09-24 22:59:10 +0800

    xx

---
+++ Solution1/DAL/DBHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace DAL
+{
+    public class DBHelper
+    {
+        public static DataTable getDt(string strSQL)
+        {
+            string strConn = ConfigurationManager.ConnectionStrings["MyConn"].ConnectionString;
+            SqlConnection conn = new SqlConnection(strConn);
+            conn.Open();
+
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(strSQL,conn);
+            da.Fill(dt);
+
+            conn.Close();
+            return dt;
+        }
+    }
+}
