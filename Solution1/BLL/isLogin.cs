013ee41e0f6437552a3870665ba7c8914b058056
yxk123450 <1129276116@qq.com>
2016-09-27 08:37:06 +0800

    log

---
+++ Solution1/BLL/isLogin.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace BLL
+{
+    public class isLogin
+    {
+        public static DataTable login(string IDl,string pawl)
+        {
+            String strSQL = "select * from load where [user]='"+IDl+"'";
+            DataTable dt = DAL.DBHelper.getDt(strSQL);
+            return dt;
+        }
+    }
+}
